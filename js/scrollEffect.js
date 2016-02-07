f6ae3ca10b4afd6cfd6cefa4d70cf05846206096
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-02-07 01:00:56 +0100

    new website using bootstrap

---
+++ js/scrollEffect.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.scroll a').bind('click', function (event) {
+        var $anchor = $(this);
+        if ($($anchor.attr("href")).length) {
+            var element = $anchor.attr("href");
+        } else {
+            var element = "body";
+        }
+        $('body').stop().animate({
+            scrollTop: $(element).offset().top
+        }, 600, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
